894234b749fe86ed916beed53570450e0b1c848d
Nathaniel Moll <ncmoll45@gmail.com>
2026-04-26 00:23:04 -0400

    new file motif_generator.py

---
+++ simulation/motif_generator.py
@@ -0,0 +1,12 @@
+from itertools import product
+
+
+allowed_chars = "ATCG"
+
+
+def possible_motif_generator(chars, motif_length):
+    "Returns a list of all possible 4-mer end-motifs"
+    sequence = [''.join(p) for p in product(chars, repeat=motif_length)]
+    return sequence
+
+motifs = possible_motif_generator(allowed_chars, 4)
